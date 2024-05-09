972bf9f891d8813b0f4893e9dd04aa56c8aeec50
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2024-05-10 03:07:13 +0700

    push

--- js/fetch.js
+++ js/fetch.js
@@ -1,6 +1,11 @@
 import { get } from "https://bukulapak.github.io/api/process.js"; 
+import { setInner } from "https://bukulapak.github.io/element/process.js";
 let urlAPI = " https://uts-irgi2024-039e21e70d31.herokuapp.com/yeseuy";
 get(urlAPI,isiTablePemain);
 function isiTablePemain(results){
     console.log(results);
+    results.forEach(isiRow);
+}
+function isiRow(value){
+    console.log(value)
 }
