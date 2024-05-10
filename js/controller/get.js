7f4e3497703c6bd886c478786e1815daac048272
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2024-05-10 10:10:46 +0700

    push

--- js/controller/get.js
+++ js/controller/get.js
@@ -8,7 +8,7 @@ export function isiTablePresensi(results) {
 function isiRow(value){
     let content = 
     isiTabel.replace("#ID#", value._id)
-            .replace("#Nama Pemain#", value.Nama_Pemain)
+            .replace("#Nama Pemain#", value.nama)
             .replace("#Posisi#", value.posisi)
             .replace("#Tinggi#", value.tinggi)
             .replace("#Berat#", value.berat)
@@ -16,16 +16,5 @@ function isiRow(value){
             .replace("#Negara#", value.negara)
             .replace("#No Punggung#", value.no_punggung );
             const tableBody = document.querySelector('#iniTabel tbody');
-            if (tableBody) {
-                // Create a new row element
-                let newRow = document.createElement('tr');
-                newRow.innerHTML = content; // Set content to the new row
-                tableBody.appendChild(newRow); // Append the new row to the tbody
-            }
-        }
-        function clearTable() {
-            const tableBody = document.querySelector('#iniTabel tbody');
-            if (tableBody) {
-                tableBody.innerHTML = ''; // Clear the inner HTML of the tbody
-            }
-        }
+            addInner("iniTabel", content);
+}
