a9bdb329f2c24acdb3bf6a77e6e4dcaa285a2f14
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2024-05-10 17:00:57 +0700

    push

--- js/controller/get.js
+++ js/controller/get.js
@@ -1,20 +1,33 @@
-import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { isiTabel } from "../temp/table.js";
 
-export function isiTablePresensi(results) {
+export function isiTable(results) {
+    clearTable();
+    console.log(results);
     results.forEach(isiRow);
 }
 
-function isiRow(value){
+function isiRow(value) {
     let content = 
-    isiTabel.replace("#Id#", value._id)
-            .replace("#Nama#", value.nama)
-            .replace("#Posisi#", value.posisi)
-            .replace("#Tinggi#", value.tinggi)
-            .replace("#Berat#", value.berat)
-            .replace("#Tanggal Lahir#", value.tanggal_lahir)
-            .replace("#Negara#", value.negara)
-            .replace("#No Punggung#", value.no_punggung );
-            
-            addInner("iniTabel", content);
+    isiTabel.replace("#Id", value._id)
+            .replace("#Nama", value.nama)
+            .replace("#Posisi", value.posisi)
+            .replace("#Tinggi", value.tinggi)
+            .replace("#Berat", value.berat)
+            .replace("#TanggalLahir", value.tanggal_lahir)
+            .replace("#Negara", value.negara)
+            .replace("#NoPunggung", value.no_punggung );
+
+    const tableBody = document.querySelector('#iniTabel tbody');
+    if (tableBody) {
+        let newRow = document.createElement('tr');
+        newRow.innerHTML = content;
+        tableBody.appendChild(newRow); 
+    }
+}
+
+function clearTable() {
+    const tableBody = document.querySelector('#iniTabel tbody');
+    if (tableBody) {
+        tableBody.innerHTML = ''; 
+    }
 }
--- js/temp/table.js
+++ js/temp/table.js
@@ -1,16 +1,30 @@
 export let isiTabel = `
   <tbody>
-    <thead>
-        <tr>
-            <th>#Id#</th>
-            <th>#Nama#</th>
-            <th>#Posisi#</th>
-            <th>#Tinggi#</th>
-            <th>#Berat#</th>
-            <th>#Tanggal Lahir#</th>
-            <th>#Negara#</th>
-            <th>#No Punggung#</th>
-        </tr>
-    </thead>
-    </tbody>   
+            <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    #Id
+                </th>
+                <td class="px-6 py-4">
+                    #Nama
+                </td>
+                <td class="px-6 py-4">
+                    #Posisi
+                </td>
+                <td class="px-6 py-4">
+                    #Tinggi
+                </td>
+                <td class="px-6 py-4">
+                    #Berat
+                </td>
+                <td class="px-6 py-4">
+                    #TanggalLahir
+                </td>
+                <td class="px-6 py-4">
+                    #Negara
+                </td>
+                <td class="px-6 py-4">
+                    #NoPunggung
+                </td>
+            </tr>
+        </tbody>
 `;
