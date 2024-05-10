2bfd91905d300a71b407440fbdb4472fc98acd16
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2024-05-10 10:31:22 +0700

    push

--- js/controller/get.js
+++ js/controller/get.js
@@ -1,26 +1,27 @@
 import { isiTabel } from "../temp/table.js";
-
-// Import fungsi clearTable jika diperlukan
+import { addInner } from "https://bukulapak.github.io/element/process.js";
 
 export function isiTablePresensi(results) {
-    clearTable(); // Panggil fungsi clearTable sebelum mengisi kembali tabel
+    clearTable();
     results.forEach(isiRow);
 }
 
 function isiRow(value){
     let content = 
-    isiTabel.replace("#ID#", value._id)
-            .replace("#Nama Pemain#", value.nama)
+    isiTabel.replace('<div class="text-transparent h-2 animate-pulse bg-slate-200 rounded col-span-2">#ID#</div>', value.id)
+            .replace('<div class="text-transparent h-2 animate-pulse bg-slate-200 rounded col-span-2">#Nama Pemain#</div>', value.nama)
             .replace("#Posisi#", value.posisi)
             .replace("#Tinggi#", value.tinggi)
             .replace("#Berat#", value.berat)
             .replace("#Tanggal Lahir#", value.tanggal_lahir)
             .replace("#Negara#", value.negara)
             .replace("#No Punggung#", value.no_punggung );
-    const tableBody = document.querySelector('#iniTabel tbody');
-    addInner("iniTabel", content);
+            const tableBody = document.querySelector('#iniTabel tbody');
+            addInner("iniTabel", content);
 }
 function clearTable() {
-    const tableBody = document.querySelector('#iniTabel tbody');
-    tableBody.innerHTML = ''; // Menghapus semua isi dari elemen tbody
+    const tableBody = document.querySelector('#isiTabel tbody');
+    if (tableBody) {
+        tableBody.innerHTML = '';
+    }
 }
