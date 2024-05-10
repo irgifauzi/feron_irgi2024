12fe5760ef080ebb4618a2e68a24e5cff333998a
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2024-05-10 13:18:45 +0700

    push

--- js/temp/table.js
+++ js/temp/table.js
@@ -1,12 +1,29 @@
 export let isiTabel = `
-  </th>
-    <th scope="row">#ID</th>
-    <th>#Nama Pemain</th>
-    <th>#Posisi</th>
-    <th>#Tinggi</th>
-    <th>#Berat</th>
-    <th>#Tanggal Lahir</th>
-    <th>#Negara</th>
-    <th>#No Punggung</th>
-  <th>
+<table>
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Nama Pemain</th>
+            <th>Posisi</th>
+            <th>Tinggi</th>
+            <th>Berat</th>
+            <th>Tanggal Lahir</th>
+            <th>Negara</th>
+            <th>No Punggung</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        </tr>
+        <!-- Tambahkan baris lain sesuai kebutuhan -->
+    </tbody>
+</table>
 `;
