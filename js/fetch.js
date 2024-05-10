f419ef0c8e1a328bd57c750a9b24ad0dabf5f5af
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2024-05-10 09:32:23 +0700

    push

--- js/fetch.js
+++ js/fetch.js
@@ -1,5 +1,6 @@
 import { get } from "https://bukulapak.github.io/api/process.js"; 
-import { setInner } from "https://bukulapak.github.io/element/process.js";
+import { addInner } from "https://bukulapak.github.io/element/process.js";
+import { isiTabel } from "./table.js";
 let urlAPI = " https://uts-irgi2024-039e21e70d31.herokuapp.com/yeseuy";
 get(urlAPI,isiTablePemain);
 function isiTablePemain(results){
@@ -7,5 +8,14 @@ function isiTablePemain(results){
     results.forEach(isiRow);
 }
 function isiRow(value){
-    console.log(value)
+    let content = 
+    isiTabel.replace("#ID#", value._id)
+            .replace("#Nama Pemain#", value.Nama_Pemain)
+            .replace("#Posisi#", value.posisi)
+            .replace("#Tinggi#", value.tinggi)
+            .replace("#Berat#", value.berat)
+            .replace("#Tanggal Lahir#", value.tanggal_lahir)
+            .replace("#Negara#", value.negara)
+            .replace("#No Punggung#", value.no_punggung );
+        addInner("iniTabel", content);
 }
---
+++ js/table.js
@@ -0,0 +1,14 @@
+export let isiTabel = `
+<tbody>
+  <tr>
+    <th scope="row">#ID</th>
+    <td>#Nama Pemain</td>
+    <td>#Posisi</td>
+    <td>#Tinggi</td>
+    <td>#Berat</td>
+    <td>#Tanggal Lahir</td>
+    <td>#Negara</td>
+    <td>#No Punggung</td>
+  </tr>
+</tbody>
+`;
